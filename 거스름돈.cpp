5fb7d21c947d27c6477c4c67e76adf81217e42ec
kbk5675 <kbk980130@gmail.com>
2022-03-16 02:48:00 +0900

    Greedy
    Greedy algorithm

---
+++ 거스름돈.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+using namespace std;
+
+// 거스름돈
+// 카운터에서 거스름돈으로 사용할 500원, 100원, 50원, 10원이 무한히 존재.
+// 손님에게 거슬러줘야 할 돈이 N개일 때 거슬러줘야할 동전의 최소 갯수를 구하라.
+// 단, 거슬러 줘야 할 돈 N은 항상 10의 배수이다.
+
+// 8줄
+void MySolution()
+{
+	int N;
+	int cnt = 0;
+	cin >> N;
+
+	cnt = N / 500;
+	cnt += (N % 500) / 100;
+	cnt += ((N % 500) % 100) / 50;
+	cnt += ((((N % 500) % 100)) % 50) / 10;
+
+	cout << cnt;
+}
+
+
+// 8줄
+void AnserSolution()
+{
+	int N;
+	cin >> N;
+	int cnt = 0;
+
+	int arr[4] = { 500,100,50,10 };
+
+	for (int i = 0; i < 4; i++)
+	{
+		cnt += N / arr[i];
+		N %= arr[i];
+	}
+
+	cout << cnt;
+}
+
+int main()
+{
+	MySolution();
+	AnserSolution();
+
+	return 0;
+}
+
+
---
+++ 큰수의법칙.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// 첫째 줄에 N( 2<= N <= 1000), M(1 <= M <= 10000), K (1 <= K <= 10000)의 자연수가 주어지며, 각 자연수는 공백으로 구분한다.
+// 둘쨰 줄에 N개의 자연수가 주어진다. 각 자연수는 공백으로 구분한다. 단, 각각의 자연수는 1 이상 10000이하의 수로 주어진다.
+// 입력으로 주어지는 K는항상 M보다 작거나 같다.
+// (다양한 수로 이뤄진 배열이 있을때 주어진 수들을 M번 더하여 가장 큰 수를 만드는 법칙)
+// (단, 배열의 특정한 인덱스에 해당하는 수가 연속해서 K번을 초과하여 더해질 수 없는 것이 이 법칙의 특징)
+// 출력조건 : 첫째 줄에 동빈이의 큰 수의 법칙에 따라 더해진 답을 출력한다.
+
+
+// 입력 예시
+// 5  8  3
+// 2  4  5  4  6
+//출력 
+// 46
+
+void MySolution();
+void AnswerSolution();
+
+int main()
+{
+	MySolution();
+	AnswerSolution();
+
+	return 0;
+}
+
+void MySolution()
+{
+	int N, M, K;
+	int fn, sn;
+	int sum = 0;
+	int arr[1000] = { 0, };
+
+	scanf_s("%d %d %d", &N, &M, &K);
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	for (int i = 0; i < N - 1; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			if (arr[j] < arr[j + 1])
+			{
+				int tmp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = tmp;
+			}
+		}
+	}
+
+	fn = arr[0];
+	sn = arr[1];
+
+	for (int i = 0; i < M; i++)
+	{
+		if (i != 0 && i / K == 0)
+			sum += sn;
+		else
+			sum += fn;
+	}
+
+	cout << sum;
+}
+
+void AnswerSolution()
+{
+	int N, M, K;
+	int fn, sn;
+	int sum = 0;
+	int arr[1000] = { 0, };
+
+	scanf_s("%d %d %d", &N, &M, &K);
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + N);
+
+	fn = arr[N - 1];
+	sn = arr[N - 2];
+
+
+	int cnt = int(M / (K + 1)) * K;
+	cnt += M % (K + 1);
+
+	sum = 0;
+	sum += (cnt)*fn;
+	sum += (M - cnt) * sn;
+
+	cout << sum;
+}
